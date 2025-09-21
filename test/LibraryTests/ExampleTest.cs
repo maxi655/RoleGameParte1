1cbbc11ef6619f9ab76b2fb9bc70550d9353423c
Agustín Martínez <martinezagustin1215@gmail.com>
2025-09-21 10:57:13 -0300

    Implementación caso de prueba : Atacar_DisminuyeSaludCorrectamente()

--- test/LibraryTests/ExampleTest.cs
+++ test/LibraryTests/ExampleTest.cs
@@ -1,3 +1,5 @@
+using Library;  
+
 namespace LibraryTests;
 
 public class Tests
@@ -6,10 +8,28 @@ public class Tests
     public void Setup()
     {
     }
-
+    // Justificación:
+    // Necesario para comprobar que al atacar a otro personaje, su salud disminuye correctamente
+    // según la diferencia entre ataque y defensa.
     [Test]
-    public void Test1()
+    public void Atacar_DisminuyeSaludCorrectamente()
     {
-        Assert.Pass();
+        Elfo elfo = new Elfo("Legolas", 100);
+        Enano enano = new Enano("Gimli", 120);
+
+        Item espada = new Item("Espada", 20, 0);
+        Item armadura = new Item("Armadura", 0, 5);
+
+        elfo.Items.Add(espada);
+        elfo.Equipar(espada);
+
+        enano.Items.Add(armadura);
+        enano.Equipar(armadura);
+
+        int saludInicial = enano.ActualSalud;
+        elfo.Atacar(enano);
+
+        Assert.Less(enano.ActualSalud, saludInicial,
+            "El ataque debe reducir la salud del objetivo en función del ataque menos la defensa.");
     }
 }
