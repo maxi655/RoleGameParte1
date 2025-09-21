29ba9a1522ebb5b76579f7a7b1bc6b43edcc59a6
Trinidad Bunge <trinibunge@gmail.com>
2025-09-21 12:19:09 -0300

    Casos de prueba nuevos

--- test/LibraryTests/ExampleTest.cs
+++ test/LibraryTests/ExampleTest.cs
@@ -32,4 +32,42 @@ public class Tests
         Assert.Less(enano.ActualSalud, saludInicial,
             "El ataque debe reducir la salud del objetivo en función del ataque menos la defensa.");
     }
+    
+    [Test]
+    public void Curar_NoSuperaSaludMax()
+    {
+        Personaje heroe = new Personaje("Aragorn", 100);
+        heroe.RecibirDaño(20); // salud actual = 80
+    
+        heroe.Curar(50); // intenta curar mas de lo necesario 
+    
+        Assert.AreEqual(100, heroe.Salud);
+    }
+    
+    // Justificación: 
+    // Este test es necesario porque asegura que el método Curar respeta el tope de salud máxima. 
+    // Sin esta validación, el personaje podría terminar con más puntos de salud de los permitidos, 
+    // rompiendo la lógica del juego.
+
+    [Test]
+    public void SacarItem_EliminaDelInventarioYDesEquipa()
+    {
+        Personaje elfo = new Personaje("Legolas", 100);
+        Item arco = new Item("Arco", 10, 0);
+        elfo.AgregarItem(arco);
+        elfo.EquiparItem(arco);
+    
+        elfo.SacarItem(arco);
+    
+        Assert.IsFalse(elfo.Inventario.Contains(arco));
+        Assert.IsFalse(elfo.ItemsEquipados.Contains(arco));
+        
+    }
+    
+    // Justificación: 
+    // Este test es necesario para garantizar que al remover un ítem no queden referencias "fantasma". 
+    // Si el ítem se elimina del inventario pero sigue equipado, el personaje tendría estadísticas 
+    // alteradas de manera indebida.
+
+    
 }
